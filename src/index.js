21068a2bf9b9b151dff65ca14f668ca7819b90af
Gian Lorenzo Comandini <gian.comandini@gmail.com>
2023-03-03 11:19:59 +0100

    Inizializzato  firebase - analytics e firestore

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+
+// TODO: Spostare i dati di configurazione come variabili env
+const firebaseConfig = {
+  apiKey: "AIzaSyD-HAWTDwbIIgfJaaxF6tUXPORXO4QWAvw",
+  authDomain: "photofloyd-72ed0.firebaseapp.com",
+  projectId: "photofloyd-72ed0",
+  storageBucket: "photofloyd-72ed0.appspot.com",
+  messagingSenderId: "270577410766",
+  appId: "1:270577410766:web:52f1107cc8edc91779e69f",
+  measurementId: "G-PQ248R8CZD"
+};
+
+// Inizializzo Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
