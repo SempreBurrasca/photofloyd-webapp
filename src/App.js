f419893391931fe89b71c563ad735c8d4d600848
Gian Lorenzo Comandini <gian.comandini@gmail.com>
2023-03-03 14:05:09 +0100

    Iniziato a creare la pagina di login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import "./App.css";
 import Login from "./Routes/Login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<h1>ciao</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Login />} />
         </Routes>
       </div>
--- src/Routes/Login/Login.js
+++ src/Routes/Login/Login.js
@@ -1,10 +1,68 @@
+import {
+  Button,
+  ButtonGroup,
+  Flex,
+  Image,
+  Switch,
+  TextField,
+  View,
+} from "@adobe/react-spectrum";
 import React from "react";
 import LayoutCentratoAlloSchermo from "../../Layouts/LayoutCentratoAlloSchermo";
 
 function Login() {
   return (
     <LayoutCentratoAlloSchermo>
-      <h1>LOGIN</h1>
+      <View borderWidth="thin" borderColor="dark" borderRadius="medium">
+        <Flex height={"50vh"} width={"70vw"}>
+          <View flex={1}>
+            <Flex width={"100%"} height={"100%"}>
+              <Image
+                src="https://i.imgur.com/Z7AzH2c.png"
+                alt="Sky and roof"
+                objectFit="cover"
+              />
+            </Flex>
+          </View>
+          <View flex={1}>
+            <Flex
+              height={"100%"}
+              direction="column"
+              alignItems={"center"}
+              justifyContent={"space-evenly"}
+              gap="size-350"
+            >
+              <h1>Login</h1>
+              <Flex
+                direction="column"
+                alignItems={"start"}
+                gap="size-150"
+                width={"70%"}
+              >
+                <TextField
+                  isRequired
+                  flex={1}
+                  label="E-mail"
+                  type="email"
+                  width={"100%"}
+                />
+                <TextField
+                  isRequired
+                  flex={1}
+                  label="Password"
+                  type={"password"}
+                  width={"100%"}
+                />
+                <Switch flex={1}>Ricordami al prossimo accesso</Switch>
+              </Flex>
+              <ButtonGroup>
+                <Button variant="primary">Recupera Password</Button>
+                <Button variant="accent">Accedi</Button>
+              </ButtonGroup>
+            </Flex>
+          </View>
+        </Flex>
+      </View>
     </LayoutCentratoAlloSchermo>
   );
 }
